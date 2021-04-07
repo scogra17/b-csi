e161b389a07a50877921203fb05b493ad54137d1
Scott Graham <scttgrhm7+public@gmail.com>
2021-04-07 10:23:40 -0700

    Optimize sum to n

--- systems/assembly-prework/sum_to_n.asm
+++ systems/assembly-prework/sum_to_n.asm
@@ -2,11 +2,10 @@ section .text
 	global sum_to_n
 
 sum_to_n:
-	xor r11, r11  ; set r11 to 0, which will be used to store the running total
+	xor rax, rax  ; set rax to 0, which will be used to store the running total
 .loop: 
-	add r11, rdi  ; add value to running total 
+	add rax, rdi  ; add value to running total 
 	sub rdi, 1    ; decrement n
-	cmp rdi, 0    ; check if n is equal to 0 
+	cmp rdi, 0    ; check if n is equal to 0. NOTE that we could exclude this step as sub will have the same effect on the carry flag https://www.aldeid.com/wiki/X86-assembly/Instructions/cmp
 	jge .loop     ; if n is not equal to 0, re-run the loop
-	mov rax, r11  ; move the total into the return register rax
 	ret
