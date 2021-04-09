4c415d7605e88824e12f27c2ceb417402a2d1137
Scott Graham <scttgrhm7+public@gmail.com>
2021-04-08 20:44:02 -0700

    Add cone volume

--- systems/assembly-2-prework/cone.asm
+++ systems/assembly-2-prework/cone.asm
@@ -3,4 +3,12 @@ default rel
 section .text
 global volume
 volume:
+ 	mulss xmm0, xmm0    ; radius^2
+ 	mulss xmm0, [pi]    ; multiply by pi
+ 	divss xmm0, [three] ; divide by 3 
+ 	mulss xmm0, xmm1    ; multiply by height 
  	ret
+
+section .data 
+pi:    dd 3.141592654
+three: dd 3.0
